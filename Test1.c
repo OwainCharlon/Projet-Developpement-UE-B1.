b08d8d180a2db2dd5c5b822e2d15135a0ddc00ba
Ynovbanj0 <paul.bagnis@gmail.com>
2019-02-28 16:53:09 +0000

    Premier push de dev

---
+++ Test1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include "menu.h"
+
+int main()
+    {
+        ecran_acceuil();
+    }
---
+++ bddplantes.h
@@ -0,0 +1,422 @@
+char choixuser[1] = {0};
+
+void viornelantane();
+void sureaunoir();
+void saulepourpre();
+void sauleaoreille();
+void sauleblanc();
+void roncebleu();
+void rosierpimprenelle();
+void rosierdeschamps();
+void bourdaine();
+void lierregrimp();
+void noisetier();
+void buis();
+void erable();
+void Achilleesternutatoire();
+void buglerampante();
+void ailjaune();
+void guimauve();
+void anemone();
+void fougere();
+void paquerette();
+void amourette();
+void populage();
+void muguet();
+void digitalepourpre();
+void fougeremale();
+void reinedespres();
+void asperuleodorante();
+void lierreterrestre();
+void houblon();
+void irisfauxacore();
+
+void erable()
+{
+    printf("%s", "\n\n  Bienvenue sur la page de l'Erable Champetre (Acer Campestre).\n");
+    printf("%s", "  Son substrat favori : non specifique.\n");
+    printf("%s", "  Son besoin en eau : |---|-------| 3\n");
+    printf("%s", "  Son besoin en soleil : |-------|---| 7\n");
+    printf("%s", "  Son besoin en pH : |----------|----| 10\n\n\n");
+    printf("%s", "Tapez Entree pour revenir a la liste des plantes.");
+    while (choixuser != '\n')
+        scanf("%c", &choixuser);
+    void menuchoixplantesbdd();
+}
+
+void buis()
+{
+    printf("%s", "\n\n  Bienvenue sur la page du Buis (Buxus sempervirens).\n");
+    printf("%s", "  Son substrat favori : pierreux.\n");
+    printf("%s", "  Son besoin en eau : |-----|-----| 5\n");
+    printf("%s", "  Son besoin en soleil : |----|------| 4\n");
+    printf("%s", "  Son besoin en pH : |--------------|| 14\n\n\n");
+    printf("%s", "Tapez Entree pour revenir a la liste des plantes.");
+    while (choixuser != '\n')
+        scanf("%c", &choixuser);
+    void menuchoixplantesbdd();
+}
+
+void noisetier()
+{
+    printf("%s", "\n\n  Bienvenue sur la page du Noisetier (Corylus avellana).\n");
+    printf("%s", "  Son substrat favori : non specifique.\n");
+    printf("%s", "  Son besoin en eau : |----|------| 4\n");
+    printf("%s", "  Son besoin en soleil : |-----|-----| 5\n");
+    printf("%s", "  Son besoin en pH : |-------|-------| 7\n\n\n");
+    printf("%s", "Tapez Entree pour revenir a la liste des plantes.");
+    while (choixuser != '\n')
+        scanf("%c", &choixuser);
+    void menuchoixplantesbdd();
+}
+
+void lierregrimp()
+{
+    printf("%s", "\n\n  Bienvenue sur la page du Lierre Grimpant (Hedera Helix).\n");
+    printf("%s", "  Son substrat favori : non specifique.\n");
+    printf("%s", "  Son besoin en eau : |---|-------| 3\n");
+    printf("%s", "  Son besoin en soleil : |-------|---| 7\n");
+    printf("%s", "  Son besoin en pH : |---------|----| 10\n\n\n");
+    printf("%s", "Tapez Entree pour revenir a la liste des plantes.");
+    while (choixuser != '\n')
+        scanf("%c", &choixuser);
+    void menuchoixplantesbdd();
+}
+
+void bourdaine()
+{
+    printf("%s", "\n\n  Bienvenue sur la page du Bourdaine (Frangula dodonei).\n");
+    printf("%s", "  Son substrat favori : non specifique.\n");
+    printf("%s", "  Son besoin en eau : |-----|-----| 5\n");
+    printf("%s", "  Son besoin en soleil : |-----|-----| 5\n");
+    printf("%s", "  Son besoin en pH : |-------|-------| 7\n\n\n");
+    printf("%s", "Tapez Entree pour revenir a la liste des plantes.");
+    while (choixuser != '\n')
+        scanf("%c", &choixuser);
+    void menuchoixplantesbdd();
+}
+
+void rosierdeschamps()
+{
+    printf("%s", "\n\n  Bienvenue sur la page du Rosier des champs (Rosa arvensis).\n");
+    printf("%s", "  Son substrat favori : non specifique.\n");
+    printf("%s", "  Son besoin en eau : |----|------| 4\n");
+    printf("%s", "  Son besoin en soleil : |-----|-----| 5\n");
+    printf("%s", "  Son besoin en pH : |-------|-------| 7\n\n\n");
+    printf("%s", "Tapez Entree pour revenir a la liste des plantes.");
+    while (choixuser != '\n')
+        scanf("%c", &choixuser);
+    void menuchoixplantesbdd();
+}
+
+void rosierpimprenelle()
+{
+    printf("%s", "\n\n  Bienvenue sur la page du Rosier Pimprenelle (Rosa pimpinellifolia).\n");
+    printf("%s", "  Son substrat favori : non specifique.\n");
+    printf("%s", "  Son besoin en eau : |-----|-----| 5\n");
+    printf("%s", "  Son besoin en soleil : |---------|-| 9\n");
+    printf("%s", "  Son besoin en pH : |--------------|| 14\n\n\n");
+    printf("%s", "Tapez Entree pour revenir a la liste des plantes.");
+    while (choixuser != '\n')
+        scanf("%c", &choixuser);
+    void menuchoixplantesbdd();
+}
+
+void roncebleu()
+{
+    printf("%s", "\n\n  Bienvenue sur la page de la Ronce Bleuatre (Rubus caesius).\n");
+    printf("%s", "  Son substrat favori : riche en elements nutritifs.\n");
+    printf("%s", "  Son besoin en eau : |----|------| 4\n");
+    printf("%s", "  Son besoin en soleil : |------|---| 7\n");
+    printf("%s", "  Son besoin en pH : |-------|-------| 7\n\n\n");
+    printf("%s", "Tapez Entree pour revenir a la liste des plantes.");
+    while (choixuser != '\n')
+        scanf("%c", &choixuser);
+    void menuchoixplantesbdd();
+}
+
+void sauleblanc()
+{
+    printf("%s", "\n\n  Bienvenue sur la page de la Saule Blanc (Salix alba).\n");
+    printf("%s", "  Son substrat favori : non specifique.\n");
+    printf("%s", "  Son besoin en eau : |-----|-----| 3\n");
+    printf("%s", "  Son besoin en soleil : |----------|| 10\n");
+    printf("%s", "  Son besoin en pH : |-------|-------| 7\n\n\n");
+    printf("%s", "Tapez Entree pour revenir a la liste des plantes.");
+    while (choixuser != '\n')
+        scanf("%c", &choixuser);
+    void menuchoixplantesbdd();
+}
+
+void sauleaoreille()
+{
+    printf("%s", "\n\n  Bienvenue sur la page de la Saule a oreilles (Salix aurita).\n");
+    printf("%s", "  Son substrat favori : non specifique.\n");
+    printf("%s", "  Son besoin en eau : |----------|| 10\n");
+    printf("%s", "  Son besoin en soleil : |-------|---| 7\n");
+    printf("%s", "  Son besoin en pH : ||--------------| 1\n\n\n");
+    printf("%s", "Tapez Entree pour revenir a la liste des plantes.");
+    while (choixuser != '\n')
+        scanf("%c", &choixuser);
+    void menuchoixplantesbdd();
+}
+
+void saulepourpre()
+{
+    printf("%s", "\n\n  Bienvenue sur la page de la Saule pourpre.\n");
+    printf("%s", "  Son substrat favori : non specifique.\n");
+    printf("%s", "  Son besoin en eau : |-------|---| 7\n");
+    printf("%s", "  Son besoin en soleil : |---------|-| 9\n");
+    printf("%s", "  Son besoin en pH : |-------|-------| 7\n\n\n");
+    printf("%s", "Tapez Entree pour revenir a la liste des plantes.");
+    while (choixuser != '\n')
+        scanf("%c", &choixuser);
+    void menuchoixplantesbdd();
+}
+
+void sureaunoir()
+{
+    printf("%s", "\n\n  Bienvenue sur la page du Sureau noir (Sambucus nigra).\n");
+    printf("%s", "  Son substrat favori : riche en azote.\n");
+    printf("%s", "  Son besoin en eau : |----|------| 4\n");
+    printf("%s", "  Son besoin en soleil : |------|---| 7\n");
+    printf("%s", "  Son besoin en pH : |-------|-------| 7\n\n\n");
+    printf("%s", "Tapez Entree pour revenir a la liste des plantes.");
+    while (choixuser != '\n')
+        scanf("%c", &choixuser);
+    void menuchoixplantesbdd();
+}
+
+void viornelantane()
+{
+    printf("%s", "\n\n  Bienvenue sur la page du Viorne lantane.\n");
+    printf("%s", "  Son substrat favori : riche en elements nutritifs.\n");
+    printf("%s", "  Son besoin en eau : |---|-------| 3\n");
+    printf("%s", "  Son besoin en soleil : |-------|---| 7\n");
+    printf("%s", "  Son besoin en pH : |----------|----| 10\n\n\n");
+    printf("%s", "Tapez Entree pour revenir a la liste des plantes.");
+    while (choixuser != '\n')
+        scanf("%c", &choixuser);
+    void menuchoixplantesbdd();
+}
+
+void Achilleesternutatoire()
+{
+    printf("%s", "\n\n  Bienvenue sur la page de l'Achilee sternutatoire (Achillea ptarmica).\n");
+    printf("%s", "  Son substrat favori : non specifique.\n");
+    printf("%s", "  Son besoin en eau : |-----|-----| 5\n");
+    printf("%s", "  Son besoin en soleil : |---------|-| 9\n");
+    printf("%s", "  Son besoin en pH : |-------|-------| 7\n\n\n");
+    printf("%s", "Tapez Entree pour revenir a la liste des plantes.");
+    while (choixuser != '\n')
+        scanf("%c", &choixuser);
+    void menuchoixplantesbdd();
+}
+
+void buglerampante()
+{
+    printf("%s", "\n\n  Bienvenue sur la page de la Bugle rampante (Ajuga reptans).\n");
+    printf("%s", "  Son substrat favori : non specifique.\n");
+    printf("%s", "  Son besoin en eau : |---|-------| 3\n");
+    printf("%s", "  Son besoin en soleil : |-------|---| 7\n");
+    printf("%s", "  Son besoin en pH : |-------|-------| 7\n\n\n");
+    printf("%s", "Tapez Entree pour revenir a la liste des plantes.");
+    while (choixuser != '\n')
+        scanf("%c", &choixuser);
+    void menuchoixplantesbdd();
+}
+
+void ailjaune()
+{
+    printf("%s", "\n\n  Bienvenue sur la page de l'Ail jaune (Allium flavum).\n");
+    printf("%s", "  Son substrat favori : pierrieux ou sablocaillouteux.\n");
+    printf("%s", "  Son besoin en eau : |-|---------| 1\n");
+    printf("%s", "  Son besoin en soleil : |------|---| 7\n");
+    printf("%s", "  Son besoin en pH : |---------|----| 10\n\n\n");
+    printf("%s", "Tapez Entree pour revenir a la liste des plantes.");
+    while (choixuser != '\n')
+        scanf("%c", &choixuser);
+    void menuchoixplantesbdd();
+}
+
+void guimauve()
+{
+    printf("%s", "\n\n  Bienvenue sur la page de la Guimauve officinale (Althaea officinalis).\n");
+    printf("%s", "  Son substrat favori : non specifique.\n");
+    printf("%s", "  Son besoin en eau : |---------|-| 9\n");
+    printf("%s", "  Son besoin en soleil : |-----|-----| 5\n");
+    printf("%s", "  Son besoin en pH : |-------|-------| 7\n\n\n");
+    printf("%s", "Tapez Entree pour revenir a la liste des plantes.");
+    while (choixuser != '\n')
+        scanf("%c", &choixuser);
+    void menuchoixplantesbdd();
+}
+
+void anemone()
+{
+    printf("%s", "\n\n  Bienvenue sur la page de l'Anemone sauvage.\n");
+    printf("%s", "  Son substrat favori : non specifique.\n");
+    printf("%s", "  Son besoin en eau : |-|---------| 1\n");
+    printf("%s", "  Son besoin en soleil : |-----|-----| 5\n");
+    printf("%s", "  Son besoin en pH : |--------------|| 14\n\n\n");
+    printf("%s", "Tapez Entree pour revenir a la liste des plantes.");
+    while (choixuser != '\n')
+        scanf("%c", &choixuser);
+    void menuchoixplantesbdd();
+}
+
+void fougere()
+{
+    printf("%s", "\n\n  Bienvenue sur la page de la Fougere femelle.\n");
+    printf("%s", "  Son substrat favori : non specifique.\n");
+    printf("%s", "  Son besoin en eau : |----|------| 4\n");
+    printf("%s", "  Son besoin en soleil : ||----------| 1\n");
+    printf("%s", "  Son besoin en pH : |---|-----------| 3\n\n\n");
+    printf("%s", "Tapez Entree pour revenir a la liste des plantes.");
+    while (choixuser != '\n')
+        scanf("%c", &choixuser);
+    void menuchoixplantesbdd();
+}
+
+void paquerette()
+{
+    printf("%s", "\n\n  Bienvenue sur la page de la Paquerette.\n");
+    printf("%s", "  Son substrat favori : non specifique.\n");
+    printf("%s", "  Son besoin en eau : |---|-------| 3\n");
+    printf("%s", "  Son besoin en soleil : |---------|-| 9\n");
+    printf("%s", "  Son besoin en pH : |-------|-------| 7\n\n\n");
+    printf("%s", "Tapez Entree pour revenir a la liste des plantes.");
+    while (choixuser != '\n')
+        scanf("%c", &choixuser);
+    void menuchoixplantesbdd();
+}
+
+void amourette()
+{
+    printf("%s", "\n\n  Bienvenue sur la page de l'Amourette (Briza media).\n");
+    printf("%s", "  Son substrat favori : non specifique.\n");
+    printf("%s", "  Son besoin en eau : |-----|-----| 5\n");
+    printf("%s", "  Son besoin en soleil : |----------|| 10\n");
+    printf("%s", "  Son besoin en pH : |-----------|---| 11\n\n\n");
+    printf("%s", "Tapez Entree pour revenir a la liste des plantes.");
+    while (choixuser != '\n')
+        scanf("%c", &choixuser);
+    void menuchoixplantesbdd();
+}
+
+void populage()
+{
+    printf("%s", "\n\n  Bienvenue sur la page de la Populage des marais (Caltha palustris).\n");
+    printf("%s", "  Son substrat favori : riche en elements nutritifs.\n");
+    printf("%s", "  Son besoin en eau : |----------|| 10\n");
+    printf("%s", "  Son besoin en soleil : |------|---| 7\n");
+    printf("%s", "  Son besoin en pH : |-------|-------| 7\n\n\n");
+    printf("%s", "Tapez Entree pour revenir a la liste des plantes.");
+    while (choixuser != '\n')
+        scanf("%c", &choixuser);
+    void menuchoixplantesbdd();
+}
+
+void muguet()
+{
+    printf("%s", "\n\n  Bienvenue sur la page du Muguet (Convallaria majalis).\n");
+    printf("%s", "  Son substrat favori : non specifique.\n");
+    printf("%s", "  Son besoin en eau : |-----|-----| 5\n");
+    printf("%s", "  Son besoin en soleil : |-|---------| 1\n");
+    printf("%s", "  Son besoin en pH : |-------|-------| 7\n\n\n");
+    printf("%s", "Tapez Entree pour revenir a la liste des plantes.");
+    while (choixuser != '\n')
+        scanf("%c", &choixuser);
+    void menuchoixplantesbdd();
+}
+
+void digitalepourpre()
+{
+    printf("%s", "\n\n  Bienvenue sur la page de la Digitale pourpre (Digitalis purpurea).\n");
+    printf("%s", "  Son substrat favori : non specifique.\n");
+    printf("%s", "  Son besoin en eau : |-----|-----| 5\n");
+    printf("%s", "  Son besoin en soleil : |-------|---| 7\n");
+    printf("%s", "  Son besoin en pH : |-|-------------| 1\n\n\n");
+    printf("%s", "Tapez Entree pour revenir a la liste des plantes.");
+    while (choixuser != '\n')
+        scanf("%c", &choixuser);
+    void menuchoixplantesbdd();
+}
+
+void fougeremale()
+{
+    printf("%s", "\n\n  Bienvenue sur la page de la Fougere male (Drypteris filix-mas).\n");
+    printf("%s", "  Son substrat favori : riche en elements nutritifs.\n");
+    printf("%s", "  Son besoin en eau : |----|------| 4\n");
+    printf("%s", "  Son besoin en soleil : |-----|-----| 5\n");
+    printf("%s", "  Son besoin en pH : |-------|-------| 7\n\n\n");
+    printf("%s", "Tapez Entree pour revenir a la liste des plantes.");
+    while (choixuser != '\n')
+        scanf("%c", &choixuser);
+    void menuchoixplantesbdd();
+}
+
+void reinedespres()
+{
+    printf("%s", "\n\n  Bienvenue sur la page de la Reine des pres (Filipendukla ulmaria).\n");
+    printf("%s", "  Son substrat favori : non specifique.\n");
+    printf("%s", "  Son besoin en eau : |----------|| 10\n");
+    printf("%s", "  Son besoin en soleil : |-------|---| 7\n");
+    printf("%s", "  Son besoin en pH : |-------|-------| 7\n\n\n");
+    printf("%s", "Tapez Entree pour revenir a la liste des plantes.");
+    while (choixuser != '\n')
+        scanf("%c", &choixuser);
+    void menuchoixplantesbdd();
+}
+
+void asperuleodorante()
+{
+    printf("%s", "\n\n  Bienvenue sur la page de l'Asperule odorante (Galium odoratum).\n");
+    printf("%s", "  Son substrat favori : non specifique.\n");
+    printf("%s", "  Son besoin en eau : |---|-------| 3\n");
+    printf("%s", "  Son besoin en soleil : |-|---------| 1\n");
+    printf("%s", "  Son besoin en pH : |-------|-------| 7\n\n\n");
+    printf("%s", "Tapez Entree pour revenir a la liste des plantes.");
+    while (choixuser != '\n')
+        scanf("%c", &choixuser);
+    void menuchoixplantesbdd();
+}
+
+void lierreterrestre()
+{
+    printf("%s", "\n\n  Bienvenue sur la page du Lierre terrestre (Glechoma hederacea).\n");
+    printf("%s", "  Son substrat favori : non specifique.\n");
+    printf("%s", "  Son besoin en eau : |---|-------| 3\n");
+    printf("%s", "  Son besoin en soleil : |---|-------| 3\n");
+    printf("%s", "  Son besoin en pH : |-------|-------| 7\n\n\n");
+    printf("%s", "Tapez Entree pour revenir a la liste des plantes.");
+    while (choixuser != '\n')
+        scanf("%c", &choixuser);
+    void menuchoixplantesbdd();
+}
+
+void houblon()
+{
+    printf("%s", "\n\n  Bienvenue sur la page du Houblon bien connu des etudiants ! (Humulus lupulus).\n");
+    printf("%s", "  Son substrat favori : riche en elements nutritifs.\n");
+    printf("%s", "  Son besoin en eau : |----------|| 10\n");
+    printf("%s", "  Son besoin en soleil : |-----|-----| 5\n");
+    printf("%s", "  Son besoin en pH : |-------|-------| 7\n\n\n");
+    printf("%s", "Tapez Entree pour revenir a la liste des plantes.");
+    while (choixuser != '\n')
+        scanf("%c", &choixuser);
+    void menuchoixplantesbdd();
+}
+
+void irisfauxacore()
+{
+    printf("%s", "\n\n  Bienvenue sur la page de l'Iris faux acore (Iris pseudacorus).\n");
+    printf("%s", "  Son substrat favori : riche en elements nutritifs.\n");
+    printf("%s", "  Son besoin en eau : |----------|| 10\n");
+    printf("%s", "  Son besoin en soleil : |-------|---| 7\n");
+    printf("%s", "  Son besoin en pH : |-------|-------| 7\n\n\n");
+    printf("%s", "Tapez Entree pour revenir a la liste des plantes.");
+    while (choixuser != '\n')
+        scanf("%c", &choixuser);
+    void menuchoixplantesbdd();
+}
